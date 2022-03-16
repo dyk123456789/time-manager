f4246808c6c60097e157eebba64bae8deb7bc3f4
dyk12345 <dyutimaan.krit8@gmail.com>
2022-03-16 20:30:46 +0530

    add crontab syntax

--- reminder.bash
+++ reminder.bash
@@ -4,3 +4,6 @@ if [[ "$(xdotool getactivewindow getwindowname)" == *"Tube"* ]]; then
     notify-send exiting...
     xdotool getactivewindow key Alt+F4
 fi
+
+# syntax for crontab: */5 14-21 * * *
+# runs every 5 min from 2 pm to 9 pm
