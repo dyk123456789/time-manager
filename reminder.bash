c68f9d391e20a489df0f9da601e20afdf135c102
dyk12345 <dyutimaan.krit8@gmail.com>
2022-03-17 21:25:51 +0530

    add condition for ti,e

--- reminder.bash
+++ reminder.bash
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-if [[ "$(xdotool getactivewindow getwindowname)" == *"Tube"* ]]; then
-    notify-send exiting...
-    xdotool getactivewindow key Alt+F4
+if [[ $(date +"%H") -lt 21 ]];
+then
+    if [[ "$(xdotool getactivewindow getwindowname)" == *"Tube"* ]]; then
+        notify-send exiting...
+        xdotool getactivewindow key Alt+F4
+    fi
 fi
-
 # syntax for crontab: */5 14-21 * * *
 # runs every 5 min from 2 pm to 9 pm
